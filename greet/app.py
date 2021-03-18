c440ee164bb0a9a3a5aca0a98e859e4db8cebf3c
thatguyish <ishmell.ramirez@gmail.com>
2021-03-18 18:12:09 -0400

    finished greet exersize

--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome():
+    return "<h1>Welcome</h1>"
+
+
+@app.route("/welcome/home")
+def welcome_home():
+    return "<h1>Welcome Home</h1>"
+
+@app.route("/welcome/back")
+def welcome_back():
+    return "<h1>Welcome Back</h1>"
+
